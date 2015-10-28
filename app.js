561915dd90f04afc93b8b7220c91c2410f2b8df9
青柳恭義 <Takayoshi.Aoyagi@gmail.com>
2015-10-29 00:26:18 +0900

    201510

--- app.js
+++ app.js
@@ -1,34 +1,6 @@
-angular.module('app', []).controller('ScheduleController', function() {
+angular.module('app', []).controller('ScheduleController', function () {
     var scope = this;
     scope.sched_list = [
-	{
-	    date: "9/27(日)",
-	    time: "13:30-17:00",
-	    name: "ライブ in ヘイワールド",
-	    desc: "",
-	    charge: 0
-	},
-	{
-	    date: "10/4(日)",
-	    time: "14:00-17:00",
-	    name: "ジャムセッション",
-	    desc: "Ba)御園生弘　Pf)沢本明男",
-	    charge: 1000
-	},
-	{
-	    date: "10/11(日)",
-	    time: "13:30-17:00",
-	    name: "ライブ in ヘイワールド",
-	    desc: "",
-	    charge: 0
-	},
-	{
-	    date: "10/18(日)",
-	    time: "14:00-17:00",
-	    name: "ジャムセッション",
-	    desc: "Pf)安ヵ川真理　Ba)本間幸夫",
-	    charge: 1300
-	},
 	{
 	    date: "10/24(土)",
 	    time: "19:00-22:00",
@@ -42,7 +14,27 @@ angular.module('app', []).controller('ScheduleController', function() {
 	    name: "ライブ in ヘイワールド",
 	    desc: "",
 	    charge: 0
+	},
+	{
+	    date: "11/1(日)",
+	    time: "14:00-17:00",
+	    name: "ジャムセッション",
+	    desc: "Pf)沢本明男　Ba)御園生弘",
+	    charge: 1000
+	},
+	{
+	    date: "11/8(日)",
+	    time: "13:30-17:00",
+	    name: "ライブ in ヘイワールド",
+	    desc: "",
+	    charge: 0
+	},
+	{
+	    date: "11/15(日)",
+	    time: "14:00-17:00",
+	    name: "ジャムセッション",
+	    desc: "Pf)安ヵ川真理　Ba)本間幸夫",
+	    charge: 1300
 	}
-
     ];
 });
