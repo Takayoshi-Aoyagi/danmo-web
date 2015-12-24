b8600b5951b32f72b9664e9ab3dc355df3345f1b
青柳恭義 <Takayoshi.Aoyagi@gmail.com>
2015-12-25 01:17:49 +0900

    20151225

--- app.js
+++ app.js
@@ -2,27 +2,6 @@ angular.module('app', []).controller('ScheduleController', function () {
     var scope = this;
     scope.sched_list = [
 	{
-	    date: "11/21(土)",
-	    time: "19:00-22:00",
-	    name: "ダンモライブ",
-	    desc: "Gt)須之内文典　Gt)桜井和樹　Ba)畑中利文",
-	    charge: 1000
-	},
-	{
-	    date: "11/22(日)",
-	    time: "13:30-17:00",
-	    name: "ライブ in ヘイワールド",
-	    desc: "",
-	    charge: 0
-	},
-	{
-	    date: "11/28(土)",
-	    time: "19:00-22:00",
-	    name: "ジャムセッション",
-	    desc: "Pf)菊谷拓也　Ba)畑中利文",
-	    charge: 1000
-	},
-	{
 	    date: "12/6(日)",
 	    time: "14:00-17:00",
 	    name: "ジャムセッション",
@@ -41,6 +20,13 @@ angular.module('app', []).controller('ScheduleController', function () {
 	    time: "14:00-17:00",
 	    name: "ジャムセッション",
 	    desc: "Pf)菊谷拓也　Ba)本間幸夫",
+	    charge: 1000
+	},
+	{
+	    date: "12/26(土)",
+	    time: "19:00-22:00",
+	    name: "ジャムセッション",
+	    desc: "Pf)菊谷拓也　Ba)畑中利文",
 	    charge: 1000
 	},
 	{
